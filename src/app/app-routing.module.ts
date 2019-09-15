dba2706cef8ba6fd3c78badde79f5acbb6871761
Guillermo De La Cruz <guillermo.dlco@outlook.com>
2019-09-15 17:32:51 -0500

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotesComponent } from './notes/notes.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'notes', component: NotesComponent},
+  {path: 'feedback', component: FeedbackComponent},
+  {path: '', component: NotesComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+// import { Router, Routes, RouterModule } from '@angular/router';
+
+// const appRoutes: Routes = [
+//   {path: 'notes', component: NotesComponent},
+//   {path: 'feedback', component: FeedbackComponent},
+//   {path: '', component: NotesComponent},
+//   {path: '**', component: NotFoundComponent},
+
+// ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    // RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
