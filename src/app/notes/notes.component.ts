2923c9f26493f216f5d61f2e1672c62c5a334788
Guillermo De La Cruz <guillermo.dlco@outlook.com>
2019-09-19 01:44:13 -0500

    Notes en pantalla principal

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Notebook } from './model/notebook';
+import { ApiService } from '../shared/api.service';
+import { Note } from './model/note';
 
 @Component({
   selector: 'app-notes',
@@ -6,10 +9,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+  notebooks: Notebook[] = [];
+  notes: Note[] = [];
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService
+  ) { }
 
   ngOnInit() {
+    this.getAllNotebooks();
+    this.getAllNotes();
   }
 
+  getAllNotebooks() {
+    this.apiService.getAllNotebooks().subscribe(
+      res => {
+        this.notebooks = res;
+      },
+      err => {
+        alert('Un error ha ocurrido');
+      }
+    );
+  }
+
+  createNotebook() {
+    let newNotebook: Notebook = {
+      name: 'New notebook',
+      id: null,
+      nbOfNotes: 0
+    }
+
+    this.apiService.postNotebook(newNotebook).subscribe(
+      res => {
+        //colocamos el id que esta viniendo del servidor
+        newNotebook.id = res.id;
+        this.notebooks.push(newNotebook);
+      },
+      err => { alert('Un error ha ocurrido mientras se guardaba el Notebook'); }
+    );
+  }
+
+  updateNotebook(updatedNotebook: Notebook) {
+    this.apiService.postNotebook(updatedNotebook).subscribe(
+      res => {
+      },
+      err => { alert('Un error ha ocurrido mientras se guardaba el Notebook'); }
+    );
+  }
+
+  deleteNotebook(notebook: Notebook) {
+    if (confirm('¿Estás seguro de eliminar la nota?')) {
+      this.apiService.deleteNotebook(notebook.id).subscribe(
+        res => {
+          let indexOfNotebook = this.notebooks.indexOf(notebook);
+          this.notebooks.splice(indexOfNotebook, 1);
+        },
+        err => { alert('No se pudo eliminar el notebook.'); }
+      );
+    }
+  }
+
+  getAllNotes(){
+    this.apiService.getAllNotes().subscribe(
+      res => {
+        this.notes = res;
+      },
+      err => {
+        alert('Ocurrio un error mientras se obtenían las notas.');
+      }
+    );
+  }
+
+  deleteNote(note: Note) {
+
+  }
 }
